3dfdbb13b05608f142ebcfc931e55c6197e8a501
Angus Panero <aguspanero@gmail.com>
2026-02-11 20:01:00 -0300

    HACER DEPLOY 4

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 dbConnection()
 
 app.use(cors({
-    origin: [ "https://deepdev-own-page-2do.onrender.com", "http://localhost:5173" ],
+    origin: [ "https://deepdev-own-page.onrender.com", "http://localhost:5173" ],
     methods: [ "GET", "POST", "PUT", "DELETE" ],
     credentials: true
 }))
